454ae566f5325662b8425bda657f0064ce5afe0a
NopoTheGamer <nopotheemail@gmail.com>
2023-09-08 09:53:47 +1000

    add comments and remove old zips in the script

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
-# This is a sample Python script.
+# Python script to download and run my game
 import os
 from zipfile import ZipFile
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def main(name):
+def main():
+    # curl downloads from urls
     os.system("curl https://download.oracle.com/java/17/latest/jdk-17_windows-x64_bin.zip -o jdk.zip")
+    # this is the direct download to my games repo
     os.system("curl https://codeload.github.com/NopoTheGamer/CardGame2/zip/refs/heads/master -o game.zip")
+    # we have to extract both zips we download
     with ZipFile('jdk.zip', 'r') as f:
         f.extractall()
     with ZipFile('game.zip', 'r') as f:
         f.extractall()
+    # Delete both the zips after extracting them as we dont need them
+    os.remove("jdk.zip")
+    os.remove("game.zip")
+    # runs java with my game as the argument
     os.system(".\\jdk-17.0.8\\bin\\javaw.exe -jar .\\CardGame2-master\\desktop-1.0.jar")
-    # extract in current directory
-    # Use a breakpoint in the code line below to debug your script.
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    main('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
